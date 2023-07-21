582429459202c9292e54e64ae5d3d8ea4a5a2d70
Vic Gurov <gurovvic@gmail.com>
2023-07-20 21:20:02 -0500

    dt 20 jul 2023

--- api_get_1_27_mar_2023.py
+++ api_get_1_27_mar_2023.py
@@ -5,7 +5,7 @@ r = requests.get('https://httpbin.org/get', params=payload)
 
 print('URL: ', r.url)
 print('ENCODING: ', r.encoding)
-print('STATUS_CODE: ',r.status_code)
+print('STATUS_CODE: ', r.status_code)
 print('HEADERS: ', r.headers)
 print('TEXT', r.text)
 print('CONTENT: ', r.content)
