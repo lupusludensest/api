f3c150fe38c737226d477e1d832e2cc20f770c96
Vic Gurov <gurovvic@gmail.com>
2023-08-12 16:40:24 -0500

    dt 12 aug 2023

---
+++ api_get_parametrized_1_11_aug_2023.py
@@ -0,0 +1,52 @@
+# import requests
+
+# class TestFirstApi:
+#     def test_hello_call(self):
+#         url = "https://playground.learnqa.ru/api/hello"
+#         name = 'Vitaly'
+#         data = {'name': name}
+#
+#         response = requests.get(url, params=data)
+#
+#         assert response.status_code == 200, "Wrong response code"
+#
+#         response_dict = response.json()
+#         assert "answer" in response_dict, "There is no field 'answer' in the response"
+#
+#         expected_response_text = f"Hello, {name}"
+#         actual_response_text = response_dict["answer"]
+#         assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
+
+# python -m pytest api_get_parametrized_1_11_aug_2023.py
+import pytest
+import requests
+
+class TestFirstApi:
+    names = [
+        ("Vitaly"),
+        ("Arseny"),
+        (""),
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
+        url = "https://playground.learnqa.ru/api/hello"
+        data = {'name': name}
+
+        response = requests.get(url, params=data)
+
+        assert response.status_code == 200, "Wrong response code"
+
+        response_dict = response.json()
+        assert "answer" in response_dict, "There is no field 'answer' in the response"
+
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
+
+        actual_response_text = response_dict["answer"]
+        assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
+
+
+
